e7bfe7ecd56145d73bb5281c167dc0101670f20f
Arthur Small <arthur.small@ventirisk.com>
2012-12-09 23:27:21 -0500

    Converts text dates + times from raw data file into date-times in the POSIXlt class.

--- load_forecasting.R
+++ load_forecasting.R
@@ -2,18 +2,16 @@
 file <- "./Data-GFS/temperature_forecasts.csv"
 raw.gfs <- read.csv(file, sep=",")
 # names(raw_data) <- c("VAR1","VAR2","RESPONSE1")
+#
 
 #  Perform transformations on data:
 #   - Convert text dates + times into date-times in the POSIXlt class
-gfs.issued.datetime <- strptime(raw.gfs[[1]], format="%m/%d/%y %H:%M", tz="GMT")
-str(gfs.issued.datetime)
+gfs.issued.datetime <- strptime(raw.gfs[[1]], format="%m/%d/%y %I:%M %p", tz="GMT")
+gfs.valid.datetime <- strptime(raw.gfs[[2]], format="%m/%d/%y %I:%M %p", tz="GMT")
 
-gfs.valid.datetime<- strptime(raw.gfs[[2]], format="%m/%d/%y %H:%M %p", tz="GMT")
-str(gfs.valid.datetime)
-sapply(gfs.valid.datetime, class)
-
-gfs.valid.datetime$hour[1:10]
 #   - Assemble transformed columns into a new data frame
+
+forecasts <- c(gfs.issued.datetime, gfs.valid.datetime, raw.gfs[[3]])
 
 head(raw.gfs)
 summary(temperature.forecasts)
