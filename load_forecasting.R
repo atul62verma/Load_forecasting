8f47cec522c2905451406c5b432faac273badb8b
Arthur Small <arthur.small@ventirisk.com>
2012-12-09 23:40:11 -0500

    Assembles transformed data series into "forecasts" data frame.

--- load_forecasting.R
+++ load_forecasting.R
@@ -1,18 +1,17 @@
 #  Load GFS forecast data
 file <- "./Data-GFS/temperature_forecasts.csv"
 raw.gfs <- read.csv(file, sep=",")
-# names(raw_data) <- c("VAR1","VAR2","RESPONSE1")
-#
+#  head(raw.gfs)
 
 #  Perform transformations on data:
 #   - Convert text dates + times into date-times in the POSIXlt class
 gfs.issued.datetime <- strptime(raw.gfs[[1]], format="%m/%d/%y %I:%M %p", tz="GMT")
-gfs.valid.datetime <- strptime(raw.gfs[[2]], format="%m/%d/%y %I:%M %p", tz="GMT")
-
+gfs.valid.datetime  <- strptime(raw.gfs[[2]], format="%m/%d/%y %I:%M %p", tz="GMT")
+temperature.forecast <- as.integer(raw.gfs[[3]])
 #   - Assemble transformed columns into a new data frame
 
-forecasts <- c(gfs.issued.datetime, gfs.valid.datetime, raw.gfs[[3]])
+forecasts <- data.frame(gfs.issued.datetime, gfs.valid.datetime, temperature.forecast)
 
-head(raw.gfs)
-summary(temperature.forecasts)
-sapply(temperature.forecasts, class)
+summary(forecasts)
+sapply(forecasts, class)
+names(forecasts)
