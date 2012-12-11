58eedc0c97db5ec80e817e7f37ef847d45ab2b62
Arthur Small <arthur.small@ventirisk.com>
2012-12-10 23:17:24 -0500

    Fixed an issue that was causing temperatures to be scrambled.

--- load_forecasting.R
+++ load_forecasting.R
@@ -8,11 +8,17 @@ raw.gfs <- read.csv(file, sep=",")
 #   - Convert text dates + times into date-times in the POSIXlt class
 gfs.issued.datetime <- strptime(raw.gfs[[1]], format="%m/%d/%y %I:%M %p", tz="GMT")
 gfs.valid.datetime  <- strptime(raw.gfs[[2]], format="%m/%d/%y %I:%M %p", tz="GMT")
-temperature.forecast <- as.integer(raw.gfs[[3]])
+
 #   - Assemble transformed columns into a new data frame
+forecasts <- data.frame(gfs.issued.datetime, gfs.valid.datetime, raw.gfs[[3]])
+names(forecasts) <- c("Issued", "Valid", "Temperature")
 
-forecasts <- data.frame(gfs.issued.datetime, gfs.valid.datetime, temperature.forecast)
+#  Select eight weeks of data beginning July 1, 2008
 
+
+
+head(raw.gfs[[3]])
+head(forecasts[[3]])
 summary(forecasts)
 sapply(forecasts, class)
 names(forecasts)
